bd2de2cea1e5627d484f8ade14198e9aa85cd238
akakou <akakou571@gmail.com>
2017-05-06 00:49:56 +0900

    [add] widgetを並べた

--- view.py
+++ view.py
@@ -2,22 +2,64 @@
 """main_view.py
 メイン画面のView
 """
-from PyQt5.QtWidgets import QMainWindow
+import PyQt5.QtWidgets as qt
 
 
-class View(QMainWindow):
+class View(qt.QMainWindow):
     """View Class."""
 
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.mainUI()
+        # 表示
+        self.show()
+
+    def send_request(self):
+        print("hello")
 
     def initUI(self):
+        """Windowの基本情報のセット"""
         # ステータスバー
         self.statusBar().showMessage('Ready')
         # windowサイズ
         self.setGeometry(1000, 1000, 1000, 1000)
         # タイトル
         self.setWindowTitle('てぃーしーぴーくらいんとちゃん')
-        # 表示
-        self.show()
+
+    def mainUI(self):
+        """リクエストフォーム"""
+        # Widget
+        self.widget = qt.QWidget()
+
+        # Layout
+        layout_box = qt.QVBoxLayout()
+
+        # URL Label
+        url_label = qt.QLabel("ゆーあーるえる")
+        layout_box.addWidget(url_label)
+
+        # LineEdit
+        self.textbox = qt.QLineEdit(self)
+        layout_box.addWidget(self.textbox)
+
+        # Request Label
+        request_label = qt.QLabel("りくえすと")
+        layout_box.addWidget(request_label)
+
+        # EditText
+        self.text_edit = qt.QTextEdit(self)
+        layout_box.addWidget(self.text_edit)
+
+        # CheckBox
+        self.is_get_response = qt.QCheckBox('れすぽんすは受け取る？', self)
+        layout_box.addWidget(self.is_get_response)
+
+        # Button
+        send_button = qt.QPushButton('そうしんっ！', self)
+        send_button.clicked.connect(self.send_request)
+        layout_box.addWidget(send_button)
+
+        # Layout
+        self.widget.setLayout(layout_box)
+        self.setCentralWidget(self.widget)
